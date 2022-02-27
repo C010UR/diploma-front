a9a245a971205cb791f56094bf4eed0346bac4d8
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-02-28 00:37:37 +0300

    Finish request app

--- request-app/src/components/TheForm.vue
+++ request-app/src/components/TheForm.vue
@@ -1,9 +1,10 @@
 <template>
-  <el-form class="form" label-position="top" :model="form" :rules="rules" show-message>
+  <el-form ref="formRef" class="form" label-position="top" :model="form" :rules="rules">
     <el-form-item label="Ф.И.О." prop="clientName">
       <el-input
         v-model="form.clientName"
         placeholder="Иванов Иван Иванович"
+        :disabled="disable.clientName"
         maxlength="128"
         show-word-limit
         clearable
@@ -14,7 +15,8 @@
       <el-input
         v-model="form.clientPhone"
         placeholder="+375 (33) 123-12-12"
-        maxlength="128"
+        :disabled="disable.clientPhone"
+        maxlength="19"
         clearable
         style="max-width: 23ch"
       ></el-input>
@@ -23,14 +25,14 @@
       <el-select
         v-model="form.cabinet"
         placeholder="Пожалуйста, выберите кабинет"
-        :loading="cabinetLoading"
-        :disabled="cabinetDisable"
+        :loading="cabinet.loading"
+        :disabled="disable.cabinet"
         loading-text="Загрузка"
-        no-data-text="Упс. Что-то пошло не так!"
+        no-data-text="Упс! Что-то пошло не так!"
         style="width: 100%; max-width: 132ch"
       >
         <el-option
-          v-for="item in cabinetOptions"
+          v-for="item in cabinet.options"
           :key="item.value"
           :label="item.label"
           :value="item.value"
@@ -41,14 +43,14 @@
       <el-select
         v-model="form.urgency"
         placeholder="Пожалуйста, выберите срочность"
-        :loading="urgencyLoading"
-        :disabled="urgencyDisable"
+        :loading="urgency.loading"
+        :disabled="disable.urgency"
         loading-text="Загрузка"
-        no-data-text="Упс. Что-то пошло не так!"
+        no-data-text="Упс! Что-то пошло не так!"
         style="width: 100%; max-width: 132ch"
       >
         <el-option
-          v-for="item in urgencyOptions"
+          v-for="item in urgency.options"
           :key="item.value"
           :label="item.label"
           :value="item.value"
@@ -59,15 +61,15 @@
       <el-select-v2
         v-model="form.defects"
         placeholder="Пожалуйста, выберите неисправности"
-        :options="defectOptions"
-        :loading="defectLoading"
-        :disabled="defectDisable"
+        :options="defects.options"
+        :loading="defects.loading"
+        :disabled="disable.defects"
         multiple
         allow-create
         filterable
         clearable
         loading-text="Загрузка"
-        no-data-text="Упс. Что-то пошло не так!"
+        no-data-text="Упс! Что-то пошло не так!"
         style="width: 100%; max-width: 132ch"
       />
     </el-form-item>
@@ -75,78 +77,50 @@
       <el-input
         type="textarea"
         v-model="form.description"
+        :disabled="disable.description"
         placeholder="В 9.15 сломался комьютер"
         autosize
       ></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSubmit" style="margin-left: auto; margin-right: 0">
-        Отправить
-      </el-button>
+      <div style="margin-left: auto; margin-right: 0">
+        <el-button @click="resetForm()">Очистить</el-button>
+        <el-button type="primary" :loading="disable.submit" @click="submitForm()">
+          Отправить
+        </el-button>
+      </div>
     </el-form-item>
   </el-form>
 </template>
 
 <script>
+import { ElMessage } from "element-plus";
+import axios from "../axios.js";
+
 export default {
   data() {
     return {
-      cabinetOptions: [
-        {
-          label: "1",
-          value: "1"
-        },
-        {
-          label: "1",
-          value: "1"
-        },
-        {
-          label: "1",
-          value: "1"
-        },
-        {
-          label: "1",
-          value: "1"
-        }
-      ],
-      cabinetLoading: false,
-      cabinetDisable: false,
-      urgencyOptions: [
-        {
-          label: "2",
-          value: "1"
-        },
-        {
-          label: "2",
-          value: "1"
-        },
-        {
-          label: "2",
-          value: "1"
-        },
-        {
-          label: "2",
-          value: "1"
-        }
-      ],
-      urgencyLoading: false,
-      urgencyDisable: false,
-      defectOptions: [
-        {
-          label: "1",
-          value: "1"
-        },
-        {
-          label: "2",
-          value: "2"
-        },
-        {
-          label: "3",
-          value: "3"
-        }
-      ],
-      defectLoading: false,
-      defectDisable: false,
+      cabinet: {
+        options: [],
+        loading: true
+      },
+      urgency: {
+        options: [],
+        loading: true
+      },
+      defects: {
+        options: [],
+        loading: true
+      },
+      disable: {
+        clientName: false,
+        clientPhone: false,
+        cabinet: false,
+        urgency: false,
+        defects: false,
+        description: false,
+        submit: false
+      },
       form: {
         clientName: "",
         clientPhone: "",
@@ -164,18 +138,112 @@ export default {
           },
           {
             min: 6,
-            message: "Ф.И.О. должно содержать более 6 символов",
+            message: "Ф.И.О. должно содержать более 5 символов",
             trigger: "blur"
           }
         ],
         cabinet: [
           {
             required: true,
-            message: "Пожалуйста, укажите кабинет"
+            message: "Пожалуйста, укажите кабинет",
+            trigger: "change"
+          }
+        ],
+        urgency: [
+          {
+            required: true,
+            message: "Пожалуйста, укажите срочность",
+            trigger: "change"
+          }
+        ],
+        defects: [
+          {
+            required: true,
+            message: "Пожалуйста, укажите неисправности",
+            trigger: "change"
           }
         ]
       }
     };
+  },
+  methods: {
+    submitForm() {
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          this.toggleAll();
+          this.sendData();
+          return true;
+        }
+        return false;
+      });
+    },
+    resetForm() {
+      this.$refs.formRef.resetFields();
+    },
+    toggleAll() {
+      Object.keys(this.disable).forEach((key) => {
+        this.disable[key] = !this.disable[key];
+      });
+    },
+    sendData() {
+      const data = {
+        client_name: this.form.clientName,
+        client_phone: this.form.clientPhone,
+        urgency: this.form.urgency,
+        cabinet: this.form.cabinet,
+        defects: this.form.defects.sort().join("; "),
+        defect_description: this.form.description
+      };
+      axios
+        .post("/request", data)
+        .then(() => {
+          ElMessage({
+            message: "Успешно отправлено",
+            type: "success"
+          });
+          this.resetForm();
+          this.toggleAll();
+        })
+        .catch(() => {
+          ElMessage.error(
+            "Упс! Мы не смогли отправить заявку. Повторите попытку либо обратитесь к администратору"
+          );
+          this.toggleAll();
+        });
+    }
+  },
+  mounted() {
+    axios
+      .get("/request/defects")
+      .then((data) => {
+        this.defects.options = data.data.map((el) => ({ value: el, label: el }));
+        this.defects.loading = false;
+      })
+      .catch(() => {
+        this.defects.loading = false;
+        ElMessage.error("Упс! Мы не смогли загрузить неисправности. Обратитесь к администратору");
+      });
+    axios
+      .get("/request/cabinets")
+      .then((data) => {
+        console.log(data);
+        this.cabinet.options = data.data;
+        this.cabinet.loading = false;
+      })
+      .catch(() => {
+        this.cabinet.loading = false;
+        ElMessage.error("Упс! Мы не смогли загрузить кабинеты. Обратитесь к администратору");
+      });
+    axios
+      .get("/request/urgency")
+      .then((data) => {
+        this.urgency.options = data.data;
+        this.urgency.loading = false;
+      })
+      .catch(() => {
+        this.urgency.loading = false;
+        ElMessage.error("Упс! Мы не смогли загрузить срочности. Обратитесь к администратору");
+      });
   }
 };
 </script>
--- shared/components/base/baseForm.vue
+++ shared/components/base/baseForm.vue
@@ -12,8 +12,7 @@ div {
   width: 40%;
   background-color: #fff;
   border-radius: 0.5rem;
-  box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12),
-    0px 1px 5px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
   transition: 0.3s;
 }
 
