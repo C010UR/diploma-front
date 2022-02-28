8d918e6c395662897c59955684207bffd63c835c
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-03-01 00:41:47 +0300

    Create normal config

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,34 @@
-const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
 
-const isRequestApp = process.env.APP_TYPE === "request-app";
-const appDir = isRequestApp ? "request-app" : "dashboard-app";
-const pubDir = isRequestApp ? "/request/" : "/dashboard/";
+let result;
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: path.resolve(__dirname, `${appDir}/dist`),
-  publicPath: process.env.NODE_ENV === "production" ? pubDir : "/",
-  devServer: {
-    port: isRequestApp ? 8080 : 7070
-  }
-});
+if (process.env.NODE_ENV === "production") {
+  const commonConfig = {
+    publicPath: process.env.NODE_ENV === "production" ? "/public/" : "/",
+    transpileDependencies: true
+  };
+  result = defineConfig({
+    pages: {
+      index: {
+        entry: "request-app/src/main.js",
+        filename: "index.html",
+        title: "Завка на ремонт",
+        ...commonConfig
+      },
+      dashboard: {
+        entry: "dashboard-app/src/main.js",
+        filename: "dashboard.html",
+        title: "Администрирование заявками на ремонт",
+        ...commonConfig
+      }
+    }
+  });
+} else {
+  result = defineConfig({
+    devServer: {
+      port: process.env.APP_TYPE === "request-app" ? 8080 : 7070
+    }
+  });
+}
+
+module.exports = result;
