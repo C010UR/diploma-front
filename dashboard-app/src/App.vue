d6de9c1f103d1c4fcc977ba17c0debe901ce7577
Mikhail Buinouskiy <verybadprogrammer@gmail.com>
2022-03-08 03:47:53 +0300

    Change the fonts

--- dashboard-app/src/App.vue
+++ dashboard-app/src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Merriweather:wght@400&family=Open+Sans:wght@400;600&display=swap");
 
 *,
 *:before,
@@ -12,12 +12,19 @@
 }
 
 body {
-  font-family: "Roboto", Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: var(--el-text-color-primary);
   background-color: var(--el-color-primary);
   margin: 0;
   padding: 0;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Merriweather", serif;
 }
 </style>
